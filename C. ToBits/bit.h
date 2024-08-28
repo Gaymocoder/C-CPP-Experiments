a91501d29541bff8788bfea8b54e7e44c271f12a
Gaymocoder <gaymocoder@gmail.com>
2024-08-28 10:48:46 +0300

    NB&&NW | Functions prototypes
    — Added prototypes of all functions into "bit.h":
      char* beginByte(void* data, size_t size);
      char* endByte(void* data, size_t size);
      void charToBits(char value, char* str);
      void toBits(void* data, size_t size, char* str);
      void printBits(void data, size_t size);
    — Added basis of "main"-function into main.c;
    
    — Doesn't build properly.

---
+++ C. ToBits/bit.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+char* beginByte(void* data, size_t size);
+char* endByte(void* data, size_t size);
+
+void charToBits(char value, char* str);
+void toBits(void* data, size_t size, char* str);
+void printBits(void data, size_t size);
---
+++ C. ToBits/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("There will be code some time\n");
+}
