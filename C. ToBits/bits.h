0f25e17fd0c5318adca3cb03ecf96a7c7225c723
Gaymocoder <gaymocoder@gmail.com>
2024-08-30 17:57:00 +0300

    NB&NW | Added .gitignore
    — Added ".gitignore" file to ignore binary files and "build" and "bin" dirs;
    
    — Renamed "bit.cpp" to "bits.c";
    — Renamed "bit.h" to "bits.h";
    
    — Doesn't build properly.

--- C. ToBits/bits.h
+++ C. ToBits/bits.h
@@ -1,8 +1,6 @@
-#include <stdio.h>
-
 char* beginByte(void* data, size_t size);
 char* endByte(void* data, size_t size);
 
 void charToBits(char value, char* str);
 void toBits(void* data, size_t size, char* str);
-void printBits(void data, size_t size);
+void printBits(void* data, size_t size);
