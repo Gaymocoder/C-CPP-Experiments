363c74710d2dc6f63871504cc92971833d75c475
Gaymocoder <gaymocoder@gmail.com>
2024-08-31 23:58:38 +0300

    B&HW | "C. ToBits": "toBits"-define
    — Added definition of "toBits"-function to "bits.c"-file;
    — The function puts in a string character-interpreted bits of data with space separator;
    — The last space of the string is been replaced with zero-terminator;
    
    — "C. 0-byte in string": builds and works properly;
    — "C. ToBits":           doesn't build.

--- C. ToBits/bits.c
+++ C. ToBits/bits.c
@@ -25,3 +25,15 @@ void charToBits(char value, char* str)
             str[i + i/8] = '0';
     }
 }
+
+void toBits(void* data, size_t size, char* str)
+{
+    char* valueByte = beginByte(data, size);
+    char* endingByte = endByte(data, size);
+    for(size_t i = 0; valueByte != endingByte; valueByte += BIGENDIAN ? 1 : -1)
+    {
+        charToBits(*valueByte, str);
+        str[++i * 9 - 1] = ' ';
+    }
+    str[size*9 -1] = 0;
+}
