0ffff974946a7aa86c594db00e61a1ad62fce9c4
Gaymocoder <gaymocoder@gmail.com>
2024-08-31 23:51:54 +0300

    B&HW | "C. ToBits": "charToBits"-define
    — Added definition of "charToBits"-function to "bits.c"-file;
    
    — "C. 0-byte in string": builds and works properly;
    — "C. ToBits":           doesn't build, doesn't work.

--- C. ToBits/bits.c
+++ C. ToBits/bits.c
@@ -14,3 +14,14 @@ char* endByte(void* data, size_t size)
         return (char*) data - 1;
     return (char*) data + size;
 }
+
+void charToBits(char value, char* str)
+{
+    for(uint8_t = 0; i < 8; ++i)
+    {
+        if (value & (1 << (8 - i - 1)))
+            str[i + i/8] = '1';
+        else
+            str[i + i/8] = '0';
+    }
+}
