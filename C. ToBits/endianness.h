0af1d5c2dc1b6becb2b4c6f3897e32f7abbb63c9
Gaymocoder <gaymocoder@gmail.com>
2024-09-02 18:04:25 +0300

    B&W | "C. ToBits": Added comments ("endianness.h")
    — Added explaining comments to "endiannes.h"-file;
    
    — "C. 0-byte in C-string": builds and works properly;
    — "C. ToBits":             builds and works properly.

--- C. ToBits/endianness.h
+++ C. ToBits/endianness.h
@@ -1,13 +1,21 @@
+// The endianness is the bytes order of multibyte data types, how they are placed in RAM
+// If first byte of data type is placed as the first one in RAM, it is called big endian
+// If first byte of data type is placed as the last one in RAM, it is called little endian
+
 #ifndef __ENDIANNESS_H__
 #define __ENDIANNESS_H__
 
 #include <stdbool.h>
 
+// Macroses which are created to indicate if endiannes is big or small (depending on "bigEndian"-variable)
 #define BIG_ENDIAN bigEndian
 #define LITTLE_ENDIAN !bigEndian
 
+// The global bool variable which is configured with start of the program
+// It indicates if the endianness is big (true) or small (false)
 extern bool bigEndian;
 
+// The function which defines the "bigEndian"-variable
 void configureEndianness();
 
 #endif
