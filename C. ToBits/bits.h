5c762d6b451ed5ab2b9b35a7cb2f12526882bf93
Gaymocoder <gaymocoder@gmail.com>
2024-09-02 18:22:11 +0300

    B&W | "C. ToBits": Added comments ("bits.h") — Added explaining comments to "bits.h"-file;
    — "C. 0-byte in C-string": builds and works properly;
    — "C. ToBits":             builds and works properly.

--- C. ToBits/bits.h
+++ C. ToBits/bits.h
@@ -3,9 +3,17 @@
 
 #include <stdint.h>
 
+// Due to existing such thing as endianness, there are to options: either first byte of data is also the first in RAM
+// or, otherwise, it is the last one in RAM. So these function are supposed to find, where are located actual first and last bytes of data
 char* beginByte(void* data, size_t size);
 char* endByte(void* data, size_t size);
 
+/*
+All bytes of data will be interpreted to bits one by one, so:
+    charToBits — transforms single byte to bits (bits are put into "str"-variable);
+    toBits — transforms all bytes of data to bits one by one with "charToBits"-function (bits are put into "str"-variable);
+    printBits — prints out bits of data got with "toBits"-function;*/
+    
 void charToBits(char value, char* str);
 void toBits(void* data, size_t size, char* str);
 void printBits(void* data, size_t size);
