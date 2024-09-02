86ba351bde9e2a718544dab900b8e09d63a0a4f5
Gaymocoder <gaymocoder@gmail.com>
2024-09-02 18:31:05 +0300

    B&W | "C. ToBits": Added comments ("endianness.c") — Added explaining comments to "endianness.c"-file;
    — "C. 0-byte in C-string": builds and works properly;
    — "C. ToBits":             builds and works properly.

--- C. ToBits/endianness.c
+++ C. ToBits/endianness.c
@@ -4,6 +4,10 @@
 
 bool bigEndian = false;
 
+// It's pretty easy to check if endiannes is big or small
+// We create a two-byted variable (uint16_t) with help of string constant
+// In the string constant there are two bytes assigned as decimal numbers: 0 and 1
+// If the first byte in memory is equeal to zero (so as in data), then the endianness is big
 void configureEndianness()
 {
     bigEndian = (*((uint16_t*) "\0\x1") == 0);
