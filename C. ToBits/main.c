f54c360f3ac22db4bafde97411c4a955ab8036d8
Gaymocoder <gaymocoder@gmail.com>
2024-09-02 11:26:07 +0300

    B&W | "C. ToBits": Added some demonstrations
    — Added demonstration of bits output for uint64_t, C-string, bool and float values;
    
    — "C. 0-byte in C-string": builds and works properly;
    — "C. ToBits":             builds and works properly.

--- C. ToBits/main.c
+++ C. ToBits/main.c
@@ -2,9 +2,27 @@
 #include "endianness.h"
 
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    uint64_t n = 1023;
-    printBits(&n, sizeof(n));
+    uint64_t int64 = 1023;
+    char* cstr = "Hello world!";
+    bool boolean = true;
+    float fl = 0.85;
+
+    printf("The bits of \"1023\"-value as uint64_t var:\n");
+    printBits(&int64, sizeof(uint64_t));
+    printf("\n");
+
+    printf("The bits of \"Hello world!\"-value as char* var:\n");
+    printBits(cstr, strlen(cstr));
+    printf("\n");
+
+    printf("The bits of \"true\"-value as bool var:\n");
+    printBits(&boolean, sizeof(bool));
+    printf("\n");
+
+    printf("The bits of \"0.85\"-value as float var:\n");
+    printBits(&fl, sizeof(fl));
 }
