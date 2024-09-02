380d8c4e0713791a86872f97575ccbfdefb9389d
Gaymocoder <gaymocoder@gmail.com>
2024-09-02 18:38:47 +0300

    B&W | "C. ToBits": Added comments ("main.c") — Added explaining comments to "main.c"-file;
    — "C. 0-byte in C-string": builds and works properly;
    — "C. ToBits":             builds and works properly.

--- C. ToBits/main.c
+++ C. ToBits/main.c
@@ -6,13 +6,16 @@
 
 int main()
 {
+    // Configuring endianness first
     configureEndianness();
     
+    // Initializing variables of different types
     uint64_t int64 = 1023;
     char* cstr = "Hello world!";
     bool boolean = true;
     float fl = 0.85;
 
+    // Printing their bits out with help of printBits
     printf("The bits of \"1023\"-value as uint64_t var:\n");
     printBits(&int64, sizeof(uint64_t));
     printf("\n");
