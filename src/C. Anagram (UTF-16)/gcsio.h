Gaymocoder/C-C-Experiments

B&W | Added experiment "C. Anagram (UTF-16)"
— Added experiment "C. Anagram (UTF-16)";
— Experiment allows to do safe c-string-input with wchar_t and reverse it;
— Experiment is cross-platformed;

— Builds and works properly.

src/C. Anagram (UTF-16)/gcsio.h
@@ -1,0 +1,15 @@
+#ifndef __GCSIO_H__
+#define __GCSIO_H__
+
+#ifndef _WIN32
+	#include <wchar.h>
+#endif
+
+void setNormalLocaleW();
+
+void clearConsole();
+void clearStdinBuf();
+
+void gcs_fgetws(wchar_t** wcstr, const size_t MAX_STR_LEN);
+
+#endif

Gaymocoder/C-CPP-Experiments

src/C. Anagram (UTF-16)/gcsio.h
@@ -1,0 +1,16 @@
+#ifndef __GCSIO_H__
+#define __GCSIO_H__
+
+#include <wchar.h>
+#ifdef _WIN32
+	#include <stddef.h>
+#endif
+
+void setNormalLocaleW();
+
+void clearConsole();
+void clearStdinBuf();
+
+void gcs_fgetws(wchar_t** wcstr, const size_t MAX_STR_LEN);
+
+#endif